f455baa5da13d7e12da5a3f8e62d06f7c8cce994
unknown <ozairk.work@gmail.com>
2026-04-07 14:14:27 +0500

    fixed db 3

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const app = require('./app')
 const connectDB = require('./db/db')
 
 
-connectDB()
-app.listen(process.env.PORT_NUM, () => {
-    console.log('Server is running')
-})
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(process.env.PORT_NUM, () => {
+            console.log('Server is running and DB is ready');
+        });
+    } catch (error) {
+        console.error('Failed to start server:', error);
+    }
+};
 
+startServer()

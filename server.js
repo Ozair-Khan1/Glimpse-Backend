aebd04b2e3de9936abed420d2bb04e79a8ab7522
unknown <ozairk.work@gmail.com>
2026-04-10 20:49:06 +0500

    removed crone

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const dotEnv = require('dotenv')
 dotEnv.config()
 const app = require('./app')
 const connectDB = require('./db/db')
-const croneJob = require('./services/crone')
 
 const startServer = async () => {
 
     try {
         await connectDB()
 
-        croneJob()
         app.listen(process.env.PORT_NUM, () => {
             console.log('Server and Db connected')
         })
